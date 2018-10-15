af44843df8514434d4eed8df5786dfd8b5bb2781
Peter Malina <peter.malina@flowup.cz>
2018-10-15 22:50:19 +0200

    cli: now uses the operator sdk to call the grpc with default host

--- cli/cmd/krane/main.go
+++ cli/cmd/krane/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"github.com/petomalina/krane/operator/pkg"
 	"github.com/urfave/cli"
+	"google.golang.org/grpc"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -10,7 +14,72 @@ func main() {
 	app.Name = "Krane"
 	app.Usage = "manipulates the krane-operator"
 
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "host, h",
+			Value: "127.0.0.1:22334",
+			Usage: "Host to connect to",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name: "create",
+			Aliases: []string{"c"},
+			Action: func(c *cli.Context) error {
+				client, err := initClient(c.String("host"))
+				if err != nil {
+					return err
+				}
+
+				client.Create(context.Background(), nil)
+
+				return nil
+			},
+		},
+		{
+			Name: "list",
+			Aliases: []string{"l"},
+			Action: func(c *cli.Context) error {
+				client, err := initClient(c.String("host"))
+				if err != nil {
+					return err
+				}
+
+				client.List(context.Background(), nil)
+
+				return nil
+			},
+		},
+		{
+			Name: "describe",
+			Aliases: []string{"d"},
+			Action: func(c *cli.Context) error {
+				client, err := initClient(c.String("host"))
+				if err != nil {
+					return err
+				}
+
+				client.Describe(context.Background(), nil)
+
+				return nil
+			},
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
 	if err := app.Run(os.Args); err != nil {
 		panic(err)
 	}
+}
+
+func initClient(host string) (operator.CanaryGatewayClient, error) {
+	gwConn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	return operator.NewCanaryGatewayClient(gwConn), nil
 }
