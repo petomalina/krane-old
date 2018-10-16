8db28c9db70760de8f02624c67b8efca3426a7d1
Peter Malina <peter.malina@flowup.cz>
2018-10-16 19:20:04 +0200

    cli: before callback is now used to setup initial values

--- cli/cmd/krane/main.go
+++ cli/cmd/krane/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"context"
+	"github.com/google/go-cloud/blob"
+	"github.com/google/go-cloud/blob/gcsblob"
+	"github.com/google/go-cloud/gcp"
 	"github.com/petomalina/krane/operator/pkg"
 	"github.com/urfave/cli"
 	"google.golang.org/grpc"
 	"os"
 	"sort"
+)
+
+var (
+	client operator.CanaryGatewayClient
+	bucket *blob.Bucket
 )
 
 func main() {
@@ -16,50 +24,52 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "host, h",
+			Name:  "host, h",
 			Value: "127.0.0.1:22334",
 			Usage: "Host to connect to",
 		},
+		cli.StringFlag{
+			Name:  "bucket, b",
+			Value: "krane-artifacts",
+			Usage: "Name of the bucket to store artifacts in",
+		},
+	}
+
+	app.Before = func(c *cli.Context) error {
+		var err error
+		client, err = initClient(c.String("host"))
+		if err != nil {
+			return err
+		}
+
+		bucket, err = setupGCP(context.Background(), c.String("bucket"))
+
+		return err
 	}
 
 	app.Commands = []cli.Command{
 		{
-			Name: "create",
+			Name:    "create",
 			Aliases: []string{"c"},
 			Action: func(c *cli.Context) error {
-				client, err := initClient(c.String("host"))
-				if err != nil {
-					return err
-				}
-
 				client.Create(context.Background(), nil)
 
 				return nil
 			},
 		},
 		{
-			Name: "list",
+			Name:    "list",
 			Aliases: []string{"l"},
 			Action: func(c *cli.Context) error {
-				client, err := initClient(c.String("host"))
-				if err != nil {
-					return err
-				}
-
 				client.List(context.Background(), nil)
 
 				return nil
 			},
 		},
 		{
-			Name: "describe",
+			Name:    "describe",
 			Aliases: []string{"d"},
 			Action: func(c *cli.Context) error {
-				client, err := initClient(c.String("host"))
-				if err != nil {
-					return err
-				}
-
 				client.Describe(context.Background(), nil)
 
 				return nil
@@ -82,4 +92,17 @@ func initClient(host string) (operator.CanaryGatewayClient, error) {
 	}
 
 	return operator.NewCanaryGatewayClient(gwConn), nil
+}
+
+func setupGCP(ctx context.Context, bucket string) (*blob.Bucket, error) {
+	creds, err := gcp.DefaultCredentials(ctx)
+	if err != nil {
+		return nil, err
+	}
+	c, err := gcp.NewHTTPClient(gcp.DefaultTransport(), gcp.CredentialsTokenSource(creds))
+	if err != nil {
+		return nil, err
+	}
+
+	return gcsblob.OpenBucket(ctx, bucket, c)
 }
